c390a3a11c9caa57249f166b785e53ab2a8df416
alex <silver398@list.ru>
2025-02-27 11:58:33 +0300

    reboot.js

--- reboot.js
+++ reboot.js
@@ -1,9 +1,10 @@
 (function() {
   'use strict';
 
+  // Функция добавления кнопки перезагрузки с обработкой событий с пульта
   function addReloadButton() {
     try {
-      // Ищем контейнер для кнопок (при необходимости измените селектор под вашу разметку)
+      // Ищем контейнер для кнопок (при необходимости измените селектор под актуальную структуру)
       var headerActions = document.querySelector('#app .head__actions');
       if (!headerActions) {
         logError('Reload Plugin Error: Контейнер ".head__actions" не найден.');
@@ -11,7 +12,7 @@
       }
       
       // HTML-разметка кнопки с иконкой SVG
-      var reloadButtonHTML = '<div id="RELOAD" class="head__action selector reload-screen">' +
+      var reloadButtonHTML = '<div id="RELOAD" class="head__action selector reload-screen" tabindex="0">' +
           '<svg fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
             '<g stroke-width="0"></g>' +
             '<g stroke-linecap="round" stroke-linejoin="round"></g>' +
@@ -24,12 +25,26 @@
       // Добавляем кнопку в найденный контейнер
       headerActions.insertAdjacentHTML('beforeend', reloadButtonHTML);
       
-      // Находим добавленную кнопку и вешаем обработчик клика
+      // Находим добавленную кнопку
       var reloadButton = document.getElementById('RELOAD');
       if (reloadButton) {
-        reloadButton.addEventListener('click', function() {
-          location.reload();
-        });
+        // Если jQuery доступен, используем его для обработки событий пульта
+        if (typeof $ !== "undefined" && typeof $(reloadButton).on === "function") {
+          $(reloadButton).on('hover:enter hover:click hover:touch', function() {
+            location.reload();
+          });
+        } else {
+          // Обработка клика
+          reloadButton.addEventListener('click', function() {
+            location.reload();
+          });
+          // Обработка клавиатурных событий для пульта (Enter или Space)
+          reloadButton.addEventListener('keydown', function(e) {
+            if (e.keyCode === 13 || e.keyCode === 32) {
+              location.reload();
+            }
+          });
+        }
       } else {
         logError('Reload Plugin Error: Кнопка не найдена после добавления.');
       }
@@ -49,7 +64,7 @@
       }
     });
   } else {
-    // Если Lampa недоступна, пробуем дождаться полной загрузки страницы
+    // Если Lampa недоступна, ждём полной загрузки страницы
     window.addEventListener('load', function() {
       addReloadButton();
     });
