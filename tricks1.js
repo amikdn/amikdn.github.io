19c2e92c010f343310667491fb7acd6cacef8ba3
alex <silver398@list.ru>
2025-03-01 21:43:50 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -383,7 +383,7 @@
           }
         };
         var fin = function fin(call) {
-          network.timeout(2000);
+          network.timeout(3000);
           network.silent(account(url), function(json) {
             life_wait_times++;
             filter_sources = [];
