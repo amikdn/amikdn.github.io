170abcbbf5495b3dcdb28543db1a804dfe192022
alex <silver398@list.ru>
2025-03-04 23:35:20 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -1,31 +1,22 @@
 (function(){
     'use strict';
     
-    // Функция для смещения панели информации на 1px вверх
-    function shiftPlayerInfo() {
-        var info = document.querySelector('.player-info.info--visible');
-        if(info){
-            // Принудительно устанавливаем относительное позиционирование
-            info.style.position = 'relative';
-            // Сдвигаем на 1px вверх
-            info.style.top = '-1px';
-        }
+    function injectCSS() {
+        var style = document.createElement('style');
+        style.id = 'custom_info_shift_css';
+        style.innerHTML = `
+            .player-info.info--visible {
+                position: relative !important;
+                top: -1px !important;
+            }
+        `;
+        document.head.appendChild(style);
     }
     
-    // Ждем, когда панель появится, и применяем смещение
-    var intervalID = setInterval(function(){
+    var checkInterval = setInterval(function(){
         if(document.querySelector('.player-info.info--visible') && document.querySelector('.player-panel.panel--visible.panel--paused')){
-            clearInterval(intervalID);
-            shiftPlayerInfo();
+            clearInterval(checkInterval);
+            injectCSS();
         }
     }, 300);
-    
-    // Если панель может динамически меняться, можно наблюдать за изменениями и постоянно применять смещение
-    var infoNode = document.querySelector('.player-info.info--visible');
-    if(infoNode){
-        var observer = new MutationObserver(function(mutations){
-            shiftPlayerInfo();
-        });
-        observer.observe(infoNode, {attributes: true, childList: true, subtree: true});
-    }
 })();
