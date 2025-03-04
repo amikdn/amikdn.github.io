439c7dea354de0b35c19a36941066af0c92347dc
alex <silver398@list.ru>
2025-03-04 23:21:35 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -1,37 +1,41 @@
 (function(){
     'use strict';
     
-    // Функция для вставки блока информации рядом с панелью плеера
+    // Функция для вставки блока информации над панелью плеера
     function insertCustomInfo() {
-        // Находим элемент панели плеера
         var panel = document.querySelector('.player-panel__position');
-        if (!panel) return;
+        if(!panel) return;
         
-        // Если блок уже добавлен, удаляем его
+        // Получаем родительский элемент панели
+        var parent = panel.parentNode;
+        // Если родитель не позиционирован, задаём ему relative
+        if(getComputedStyle(parent).position === 'static'){
+            parent.style.position = 'relative';
+        }
+        
+        // Удаляем предыдущий блок, если он уже был добавлен
         var oldInfo = document.getElementById('custom_player_info');
         if(oldInfo) oldInfo.remove();
         
-        // Формируем HTML-блок с информацией и смещением (регулируйте top по необходимости)
+        // Создаем HTML-блок с абсолютным позиционированием
+        // bottom: calc(100% + 10px) поднимает блок на 10px выше панели
         var customHTML = 
-            '<div id="custom_player_info" style="position: relative; top: -50px;">' +
+            '<div id="custom_player_info" style="position: absolute; bottom: calc(100% + 10px); left: 0; width: 100%;">' +
                 '<div class="player-info__line">' +
                     '<div class="player-info__name"></div>' +
                     '<div class="player-info__time"><span class="time--clock"></span></div>' +
                 '</div>' +
             '</div>';
         
-        // Вставляем новый блок непосредственно перед панелью плеера
-        panel.parentNode.insertBefore(
-            (function(){
-                var temp = document.createElement('div');
-                temp.innerHTML = customHTML;
-                return temp.firstChild;
-            })(),
-            panel
-        );
+        // Вставляем блок в родительский элемент перед панелью
+        parent.insertBefore((function(){
+            var temp = document.createElement('div');
+            temp.innerHTML = customHTML;
+            return temp.firstChild;
+        })(), panel);
     }
     
-    // Функция инициализации плагина с ожиданием загрузки панели плеера
+    // Функция инициализации плагина: ждем появления панели и затем вставляем блок
     function initPlugin() {
         var intervalID = setInterval(function(){
             if(document.querySelector('.player-panel__position')){
@@ -41,7 +45,7 @@
         }, 500);
     }
     
-    // Регистрируем параметр в меню настроек Lampa для управления плагином
+    // Регистрация параметра в меню настроек Lampa для управления плагином
     Lampa.SettingsApi.addParam({
         component: 'Multi_Menu_Component',
         param: {
@@ -51,7 +55,7 @@
         },
         field: {
             name: 'Замена и смещение иконок плеера',
-            description: 'Вставляет блок с информацией (player-info__line) непосредственно над панелью плеера'
+            description: 'Вставляет блок с информацией (player-info__line) над панелью плеера'
         },
         onChange: function(value) {
             if(value === true){
