c6eefc0484834ec1dc47a81f1df6741d861fe110
alex <silver398@list.ru>
2025-03-07 23:19:41 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -2,7 +2,7 @@
 (function () {
     'use strict';
     Lampa.Listener.follow('full', function (e) {
-        if (e.type == 'complete') {
+        if (e.type == 'complite') {
             e.object.activity.render().find('.view--trailer').remove();
         }
     });
