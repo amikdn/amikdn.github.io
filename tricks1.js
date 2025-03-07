09b3d8a83fd824d76cb7a0de729536257b74d02f
alex <silver398@list.ru>
2025-03-07 23:09:13 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -1,6 +1,7 @@
 
 (function () {
-    'use strict';	
+    'use strict';
+    Lampa.Platform.tv();
     Lampa.Listener.follow('full', function (e) {
         if (e.type == 'complite') {
             e.object.activity.render().find('.view--trailer').remove();
