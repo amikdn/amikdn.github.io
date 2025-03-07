d3b0f6d14276c1abdaa8ddb423b7447ad479b558
alex <silver398@list.ru>
2025-03-07 23:20:19 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -1,9 +1,14 @@
-
 (function () {
     'use strict';
     Lampa.Listener.follow('full', function (e) {
-        if (e.type == 'complite') {
-            e.object.activity.render().find('.view--trailer').remove();
+        console.log('Событие получено:', e);
+        if (e.type == 'complete') {
+            let activity = e.object.activity;
+            let rendered = activity.render();
+            console.log('Отрендеренный элемент:', rendered);
+            let trailers = rendered.find('.view--trailer');
+            console.log('Найденные трейлеры:', trailers);
+            trailers.remove();
         }
     });
 })();
