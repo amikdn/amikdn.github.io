702df8094904cd0d39b66112d65e860c54ef3ff5
alex <silver398@list.ru>
2025-04-18 22:44:57 +0300

    tricks1.js

--- tricks1.js
+++ tricks1.js
@@ -1,4 +1,4 @@
-(function () {
+;(function () {
     'use strict';
 
     Lampa.Platform.tv();
@@ -12,49 +12,57 @@
         }, _0x46dc(_0x5d37fe, _0x17b3db);
     }
 
+    // Первый IIFE: настройка сдвигов
     (function (_0x36ea34, _0x29f6d6) {
         var _0x3e9f7e = _0x46dc,
             _0x32657c = _0x36ea34();
-        while (!![]) {
+        while (true) {
             try {
-                var _0xd0bbca = -parseInt(_0x3e9f7e(0xb5)) / 0x1
-                    + parseInt(_0x3e9f7e(0x9c)) / 0x2 * (-parseInt(_0x3e9f7e(0x91)) / 0x3)
-                    + parseInt(_0x3e9f7e(0x9d)) / 0x4
-                    + -parseInt(_0x3e9f7e(0xb8)) / 0x5 * (-parseInt(_0x3e9f7e(0xb6)) / 0x6)
-                    + -parseInt(_0x3e9f7e(0xbd)) / 0x7
-                    + -parseInt(_0x3e9f7e(0x94)) / 0x8 * (parseInt(_0x3e9f7e(0xac)) / 0x9)
-                    + parseInt(_0x3e9f7e(0x9b)) / 0xa * (parseInt(_0x3e9f7e(0xa6)) / 0xb);
+                var _0xd0bbca = -parseInt(_0x3e9f7e(0xb5)) / 1
+                    + parseInt(_0x3e9f7e(0x9c)) / 2 * (-parseInt(_0x3e9f7e(0x91)) / 3)
+                    + parseInt(_0x3e9f7e(0x9d)) / 4
+                    + -parseInt(_0x3e9f7e(0xb8)) / 5 * (-parseInt(_0x3e9f7e(0xb6)) / 6)
+                    + -parseInt(_0x3e9f7e(0xbd)) / 7
+                    + -parseInt(_0x3e9f7e(0x94)) / 8 * (parseInt(_0x3e9f7e(0xac)) / 9)
+                    + parseInt(_0x3e9f7e(0x9b)) / 10 * (parseInt(_0x3e9f7e(0xa6)) / 11);
                 if (_0xd0bbca === _0x29f6d6) break;
                 else _0x32657c.push(_0x32657c.shift());
-            } catch (_0x58318a) {
+            } catch (e) {
                 _0x32657c.push(_0x32657c.shift());
             }
         }
-    }(_0x5481, 0x34f52), (function () {
+    }(_0x5481, 0x34f52));
+
+    // Второй IIFE: основная логика и запуск
+    (function () {
         var _0x27e607 = _0x46dc,
             _0xdf95c5 = (function () {
-                var _0x1ccdf0 = !![];
+                var _0x1ccdf0 = true;
                 return function (_0xb5f84e, _0xc953c1) {
                     var _0x2d531f = _0x1ccdf0 ? function () {
                         if (_0xc953c1) {
                             var _0x5dc649 = _0xc953c1.apply(_0xb5f84e, arguments);
-                            return _0xc953c1 = null, _0x5dc649;
+                            _0xc953c1 = null;
+                            return _0x5dc649;
                         }
                     } : function () {};
-                    return _0x1ccdf0 = ![], _0x2d531f;
+                    _0x1ccdf0 = false;
+                    return _0x2d531f;
                 };
             }()),
             _0x5ba9bc = (function () {
-                var _0x480539 = !![];
+                var _0x480539 = true;
                 return function (_0x326f9e, _0x87a30e) {
                     var _0x51f8e0 = _0x480539 ? function () {
                         var _0x315b7e = _0x46dc;
                         if (_0x87a30e) {
                             var _0xbe7b07 = _0x87a30e[_0x315b7e(0xb7)](_0x326f9e, arguments);
-                            return _0x87a30e = null, _0xbe7b07;
+                            _0x87a30e = null;
+                            return _0xbe7b07;
                         }
                     } : function () {};
-                    return _0x480539 = ![], _0x51f8e0;
+                    _0x480539 = false;
+                    return _0x51f8e0;
                 };
             }());
 
@@ -65,31 +73,27 @@
                     return _0x2f2962.toString().replace(_0x59e4d7(0xae)).split('').reverse().join('') === _0x2f2962.toString().replace(_0x59e4d7(0xae));
                 });
             _0x2f2962();
+
             var _0x1b988d = _0x5ba9bc(this, function () {
                 var _0x43a117 = _0x46dc,
                     _0xf8d389 = function () {
-                        var _0x3dcfea = _0x46dc,
-                            _0x31d787;
                         try {
-                            _0x31d787 = Function('return (function(){ })')();
-                        } catch (_0x1eba67) {
-                            _0x31d787 = window;
+                            return Function('return (function(){ })')();
+                        } catch {
+                            return window;
                         }
-                        return _0x31d787;
                     },
                     _0x62bba9 = _0xf8d389(),
                     _0x2feb0e = _0x62bba9.console = _0x62bba9[_0x43a117(0xa9)] || {},
                     _0x82a894 = ['log', 'warn', 'error', 'info', 'trace'];
-                for (var _0x2fb310 = 0x0; _0x2fb310 < _0x82a894.length; _0x2fb310++) {
-                    var _0x420f37 = _0x82a894[_0x2fb310],
-                        _0xa5e86c = _0x2feb0e[_0x420f37] || function () {};
+                for (var _0x2fb310 = 0; _0x2fb310 < _0x82a894.length; _0x2fb310++) {
+                    var _0x420f37 = _0x82a894[_0x2fb310];
                     _0x2feb0e[_0x420f37] = function () {};
                 }
             });
             _0x1b988d();
 
             Lampa.Listener.follow('request_secuses', function (_0x55e51a) {
-                var _0x327b9a = _0x2b9cf3;
                 if (_0x55e51a.data.success) {
                     var _0x4303a0 = Lampa.Api.active();
                     _0x4303a0.url = 'request_secuses';
@@ -99,7 +103,6 @@
             });
 
             var _0x43e9c3 = setInterval(function () {
-                var _0x5a1733 = _0x2b9cf3;
                 if (typeof window.Storage !== 'undefined' &&
                     (window.localStorage.fixdcma || window.lampa_settings.fixdcma)) {
                     clearInterval(_0x43e9c3);
@@ -108,16 +111,16 @@
                     }
                 }
             }, 0x64);
-        };
+        }
 
         if (window.appready) {
             _0x52a4d3();
         } else {
-            Lampa.Listener.follow('app:ready', function (_0x59cbfc) {
-                if (_0x59cbfc.type === 'ready') _0x52a4d3();
+            Lampa.Listener.follow('app:ready', function (e) {
+                if (e.type === 'ready') _0x52a4d3();
             });
         }
-     }());
+    }());
 
     function _0x5481() {
         var _0x92b9f2 = [
@@ -136,4 +139,4 @@
         return _0x5481();
     }
 
-})();
+}());
