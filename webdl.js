e8fe04bff0577178fb756a67a101902308211eee
alex <silver398@list.ru>
2025-08-23 10:11:43 +0300

    webdl.js

--- webdl.js
+++ webdl.js
@@ -197,7 +197,7 @@
     function startPlugin() {
         Lampa.SettingsApi.addComponent({
             component: 'torrent_quality',
-            name: 'Фильтр',
+            name: 'Фильтр WEB-DL',
             icon: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="currentColor"/><path d="M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79-4-4-4z" fill="currentColor"/></svg>'
         });
 
@@ -215,7 +215,7 @@
                 default: 'any'
             },
             field: {
-                name: 'Фильтр',
+                name: 'Фильтр WEB-DL',
                 description: 'Выберите параметры для фильтрации торрентов'
             },
             onRender: function (element) {
@@ -365,7 +365,7 @@
 
     // Манифест плагина
     Lampa.Manifest.plugins = {
-        name: 'Фильтр Торрентов',
+        name: 'Фильтр WEB-DL',
         version: '1.1.27',
         description: 'Фильтрация торрентов по качеству для текущего фильма'
     };
@@ -380,3 +380,4 @@
         });
     }
 })();
+
