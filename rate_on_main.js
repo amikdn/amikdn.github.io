14818008b043b9b2651429e69b80ca7a9e44a4cb
alex <silver398@list.ru>
2025-09-10 22:03:53 +0300

    rate_on_main.js

--- rate_on_main.js
+++ rate_on_main.js
@@ -99,7 +99,7 @@
         if (rateLine.find('.rate--lampa').length > 0) return true;
         let lampaBlockHtml =
             '<div class="full-start__rate rate--lampa">' +
-                '<div class="rate-value">0.0 LAMPA</div>' +
+                '<div class="rate-value">0 LAMPA</div>' +
                 '<div class="source--name">LAMPA</div>' +
             '</div>';
         let kpBlock = rateLine.find('.rate--kp');
@@ -118,7 +118,7 @@
             voteEl.className = 'card__vote';
             let viewEl = card.querySelector('.card__view') || card;
             viewEl.appendChild(voteEl);
-            voteEl.innerHTML = '0.0'; // Начальное значение
+            voteEl.innerHTML = '0 LAMPA'; // Начальное значение
         } else {
             voteEl.innerHTML = ''; // Очищаем предыдущий контент (TMDB)
         }
@@ -132,7 +132,7 @@
         let ratingKey = type + "_" + id;
         console.log('Rating key for card:', ratingKey, 'Card data:', { card, data, cardData, event: event.object });
         getLampaRating(ratingKey).then(rating => {
-            voteEl.innerHTML = rating !== null ? rating : '0.0';
+            voteEl.innerHTML = rating !== null ? rating : '0 LAMPA';
             console.log('Rating set to:', voteEl.innerHTML, 'for', ratingKey);
         }).catch(error => {
             console.error('Error setting rating for ' + ratingKey + ':', error);
