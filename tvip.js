6a0c3a4609746d2909ce05f727f3cc3da3aaad8d
alex <silver398@list.ru>
2025-09-14 21:07:34 +0300

    tvip.js

--- tvip.js
+++ tvip.js
@@ -3,7 +3,7 @@
 var plugin = {
     component: 'my_iptv2',
     icon: "<svg height=\"244\" viewBox=\"0 0 260 244\" xmlns=\"http://www.w3.org/2000/svg\" style=\"fill-rule:evenodd;\" fill=\"currentColor\"><path d=\"M259.5 47.5v114c-1.709 14.556-9.375 24.723-23 30.5a2934.377 2934.377 0 0 1-107 1.5c-35.704.15-71.37-.35-107-1.5-13.625-5.777-21.291-15.944-23-30.5v-115c1.943-15.785 10.61-25.951 26-30.5a10815.71 10815.71 0 0 1 208 0c15.857 4.68 24.523 15.18 26 31.5zm-230-13a4963.403 4963.403 0 0 0 199 0c5.628 1.128 9.128 4.462 10.5 10 .667 40 .667 80 0 120-1.285 5.618-4.785 8.785-10.5 9.5-66 .667-132 .667-198 0-5.715-.715-9.215-3.882-10.5-9.5-.667-40-.667-80 0-120 1.35-5.18 4.517-8.514 9.5-10z\"/><path d=\"M70.5 71.5c17.07-.457 34.07.043 51 1.5 5.44 5.442 5.107 10.442-1 15-5.991.5-11.991.666-18 .5.167 14.337 0 28.671-.5 43-3.013 5.035-7.18 6.202-12.5 3.5a11.529 11.529 0 0 1-3.5-4.5 882.407 882.407 0 0 1-.5-42c-5.676.166-11.343 0-17-.5-4.569-2.541-6.069-6.375-4.5-11.5 1.805-2.326 3.972-3.992 6.5-5zM137.5 73.5c4.409-.882 7.909.452 10.5 4a321.009 321.009 0 0 0 16 30 322.123 322.123 0 0 0 16-30c2.602-3.712 6.102-4.879 10.5-3.5 5.148 3.334 6.314 7.834 3.5 13.5a1306.032 1306.032 0 0 0-22 43c-5.381 6.652-10.715 6.652-16 0a1424.647 1424.647 0 0 0-23-45c-1.691-5.369-.191-9.369 4.5-12zM57.5 207.5h144c7.788 2.242 10.288 7.242 7.5 15a11.532 11.532 0 0 1-4.5 3.5c-50 .667-100 .667-150 0-6.163-3.463-7.496-8.297-4-14.5 2.025-2.064 4.358-3.398 7-4z\"/></svg>",
-    name: 'Hack TV'
+    name: 'Hack TV' // Название для бокового меню
 };
 
 var lists = [];
@@ -1449,12 +1449,13 @@ function addSettings(type, param) {
 
 Lampa.Component.add(plugin.component, pluginPage);
 
+// Регистрация компонента в настройках
 Lampa.SettingsApi.addComponent(plugin);
 
 // Инициализация списка плейлистов
 lists[0] = lists[0] || {
     id: 0,
-    url: getSettings('playlist_url') || '', // Загружаем URL из настроек, если он есть
+    url: getSettings('playlist_url') || '',
     groups: [],
     activity: { currentGroup: '' }
 };
@@ -1481,24 +1482,57 @@ addSettings(
                 });
             }
         }
+    },
+    {
+        name: 'settings_title',
+        title: langGet('settings_title') // Отображается как "Hack TV PlayList"
     }
 );
 
 // Регистрация пункта меню в левом боковом меню
-Lampa.Menu.add({
-    title: plugin.name, // Название плагина ("Hack TV PlayList")
-    icon: plugin.icon,  // Иконка плагина
-    component: plugin.component, // Компонент "my_iptv2"
-    action: function () {
-        // Запускаем активность с текущим URL плейлиста
-        console.log('Opening IPTV with URL:', getSettings('playlist_url')); // Для отладки
-        Lampa.Activity.push({
-            component: plugin.component,
-            id: 0,
-            url: getSettings('playlist_url') || '', // Используем URL из настроек
-            currentGroup: getStorage('last_catalog0', '') || '' // Восстанавливаем последнюю группу
-        });
-    }
-});
+if (typeof Lampa.MainMenu !== 'undefined' && typeof Lampa.MainMenu.add === 'function') {
+    Lampa.MainMenu.add({
+        title: plugin.name, // Название в боковом меню: "Hack TV"
+        icon: plugin.icon,  // Иконка плагина
+        component: plugin.component, // Компонент "my_iptv2"
+        action: function () {
+            console.log('Opening IPTV with URL:', getSettings('playlist_url')); // Для отладки
+            Lampa.Activity.push({
+                component: plugin.component,
+                id: 0,
+                url: getSettings('playlist_url') || '', // Используем URL из настроек
+                currentGroup: getStorage('last_catalog0', '') || '' // Восстанавливаем последнюю группу
+            });
+        }
+    });
+} else {
+    console.warn('Lampa.MainMenu.add is not available, falling back to alternative method');
+    // Альтернативный способ добавления пункта меню
+    Lampa.Component.add(plugin.component, pluginPage);
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type === 'ready') {
+            console.log('App ready, registering custom menu item');
+            // Попытка добавить пункт меню через другой метод, если доступен
+            if (typeof Lampa.Menu !== 'undefined' && typeof Lampa.Menu.register === 'function') {
+                Lampa.Menu.register({
+                    title: plugin.name,
+                    icon: plugin.icon,
+                    component: plugin.component,
+                    action: function () {
+                        console.log('Opening IPTV with URL:', getSettings('playlist_url'));
+                        Lampa.Activity.push({
+                            component: plugin.component,
+                            id: 0,
+                            url: getSettings('playlist_url') || '',
+                            currentGroup: getStorage('last_catalog0', '') || ''
+                        });
+                    }
+                });
+            }
+        }
+    });
+}
 
+// Завершение анонимной функции плагина
 })();
+
