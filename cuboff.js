5253737339e109080ce5315fe5ee02a68168f3c4
alex <silver398@list.ru>
2025-09-22 22:11:36 +0300

    cuboff.js

--- cuboff.js
+++ cuboff.js
@@ -1,20 +1,15 @@
-(function () {
-  'use strict';
-  Lampa.Platform.tv();
-
-  // Перехват checkPremium сразу
+function initializeApp() {
+  // Переопределяем checkPremium
   window.checkPremium = () => {
     console.log('checkPremium overridden, returning 1');
     return 1;
   };
+  // Проверяем hasPremium
   console.log('hasPremium check:', Lampa.Account.hasPremium());
 
-  // Остальной код плагина
-  function initializeApp() {
-    var style = document.createElement('style');
-    style.innerHTML = '.button--subscribe { display: none; }';
-    document.body.appendChild(style);
-    // ... и так далее
-  }
-  // ... остальной код
-})();
+  // Остальной код плагина...
+  var style = document.createElement('style');
+  style.innerHTML = '.button--subscribe { display: none; }';
+  document.body.appendChild(style);
+  // ... и так далее
+}
