5e857575156e961f4fde4d38dc5cfdf9341051a7
alex <silver398@list.ru>
2025-09-22 22:13:59 +0300

    cuboff.js

--- cuboff.js
+++ cuboff.js
@@ -1,15 +1,10 @@
 function initializeApp() {
-  // Переопределяем checkPremium
   window.checkPremium = () => {
     console.log('checkPremium overridden, returning 1');
     return 1;
   };
-  // Проверяем hasPremium
   console.log('hasPremium check:', Lampa.Account.hasPremium());
-
-  // Остальной код плагина...
   var style = document.createElement('style');
   style.innerHTML = '.button--subscribe { display: none; }';
   document.body.appendChild(style);
-  // ... и так далее
 }
