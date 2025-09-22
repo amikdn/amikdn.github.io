527cb1fe3ca4e640825278e6f9f8c5f86f339ac1
alex <silver398@list.ru>
2025-09-22 22:07:52 +0300

    cuboff.js

--- cuboff.js
+++ cuboff.js
@@ -1,11 +1,20 @@
-Lampa.Account = new Proxy(Lampa.Account, {
-  get(target, prop) {
-    if (prop === 'hasPremium') {
-      return () => {
-        console.log('hasPremium intercepted, returning 1');
-        return 1;
-      };
-    }
-    return target[prop];
+(function () {
+  'use strict';
+  Lampa.Platform.tv();
+
+  // Перехват checkPremium сразу
+  window.checkPremium = () => {
+    console.log('checkPremium overridden, returning 1');
+    return 1;
+  };
+  console.log('hasPremium check:', Lampa.Account.hasPremium());
+
+  // Остальной код плагина
+  function initializeApp() {
+    var style = document.createElement('style');
+    style.innerHTML = '.button--subscribe { display: none; }';
+    document.body.appendChild(style);
+    // ... и так далее
   }
-});
+  // ... остальной код
+})();
