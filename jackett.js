3620c204d9c422a9db6cc375c994c776dd22b90a
alex <silver398@list.ru>
2025-10-17 22:19:16 +0300

    jackett.js

--- jackett.js
+++ jackett.js
@@ -55,7 +55,7 @@
     }
 
     function checkAllParsers() {
-        for (var i = 0; i <= urls.length - 1; i++) {
+        for (var i = 0; i < urls.length; i++) {
             checkParser(i);
         }
     }
@@ -69,70 +69,75 @@
     });
 
     function setParserConfig() {
-        if (Lampa.Storage.get('jackett_urltwo') == 'no_parser') {
-            Lampa.Storage.set('jackett_url', '');
-            Lampa.Storage.set('jackett_key', '');
-            Lampa.Storage.set('jackett_interview', 'false');
-            Lampa.Storage.set('parse_in_search', false);
-            Lampa.Storage.set('parse_lang', 'lg');
-        }
+        var selected = Lampa.Storage.get('jackett_urltwo');
         
-        if (Lampa.Storage.get('jackett_urltwo') == 'jacred_xyz') {
-            Lampa.Storage.set('jackett_url', 'jacred.xyz');
-            Lampa.Storage.set('jackett_key', '');
-            Lampa.Storage.set('jackett_interview', 'healthy');
-            Lampa.Storage.set('parse_in_search', true);
-            Lampa.Storage.set('parse_lang', 'lg');
-        }
-        
-        if (Lampa.Storage.get('jackett_urltwo') == 'jr_maxvol_pro') {
-            Lampa.Storage.set('jackett_url', 'jr.maxvol.pro');
-            Lampa.Storage.set('jackett_key', '');
-            Lampa.Storage.set('jackett_interview', 'all');
-            Lampa.Storage.set('parse_in_search', true);
-            Lampa.Storage.set('parse_lang', 'df');
-        }
-        
-        if (Lampa.Storage.get('jackett_urltwo') == 'jacred_ru') {
-            Lampa.Storage.set('jackett_url', 'jac-red.ru');
-            Lampa.Storage.set('jackett_key', '');
-            Lampa.Storage.set('jackett_interview', 'false');
-            Lampa.Storage.set('parse_in_search', true);
-            Lampa.Storage.set('parse_lang', 'lg');
-        }
-        
-        if (Lampa.Storage.get('jackett_urltwo') == 'jacred_pro') {
-            Lampa.Storage.set('jackett_url', 'jacred.pro');
-            Lampa.Storage.set('jackett_key', '');
-            Lampa.Storage.set('jackett_interview', 'all');
-            Lampa.Storage.set('parse_in_search', true);
-            Lampa.Storage.set('parse_lang', 'lg');
-        }
-        
-        if (Lampa.Storage.get('jackett_urltwo') == 'jac_black') {
-            Lampa.Storage.set('jackett_url', 'jacblack.ru:9117');
-            Lampa.Storage.set('jackett_key', '');
-            Lampa.Storage.set('jackett_interview', 'false');
-            Lampa.Storage.set('parse_in_search', true);
-            Lampa.Storage.set('parse_lang', 'lg');
-        }
-        
-        if (Lampa.Storage.get('jackett_urltwo') == 'jacred_viewbox_dev') {
-            Lampa.Storage.set('jackett_url', 'jacred.viewbox.dev');
-            Lampa.Storage.set('jackett_key', '777');
-            Lampa.Storage.set('jackett_interview', 'false');
-            Lampa.Storage.set('parse_in_search', true);
-            Lampa.Storage.set('parse_lang', 'lg');
+        switch(selected) {
+            case 'no_parser':
+                Lampa.Storage.set('jackett_url', '');
+                Lampa.Storage.set('jackett_key', '');
+                Lampa.Storage.set('jackett_interview', 'false');
+                Lampa.Storage.set('parse_in_search', false);
+                Lampa.Storage.set('parse_lang', 'lg');
+                break;
+                
+            case 'jacred_xyz':
+                Lampa.Storage.set('jackett_url', 'jacred.xyz');
+                Lampa.Storage.set('jackett_key', '');
+                Lampa.Storage.set('jackett_interview', 'healthy');
+                Lampa.Storage.set('parse_in_search', true);
+                Lampa.Storage.set('parse_lang', 'lg');
+                break;
+                
+            case 'jr_maxvol_pro':
+                Lampa.Storage.set('jackett_url', 'jr.maxvol.pro');
+                Lampa.Storage.set('jackett_key', '');
+                Lampa.Storage.set('jackett_interview', 'all');
+                Lampa.Storage.set('parse_in_search', true);
+                Lampa.Storage.set('parse_lang', 'df');
+                break;
+                
+            case 'jacred_ru':
+                Lampa.Storage.set('jackett_url', 'jac-red.ru');
+                Lampa.Storage.set('jackett_key', '');
+                Lampa.Storage.set('jackett_interview', 'false');
+                Lampa.Storage.set('parse_in_search', true);
+                Lampa.Storage.set('parse_lang', 'lg');
+                break;
+                
+            case 'jacred_pro':
+                Lampa.Storage.set('jackett_url', 'jacred.pro');
+                Lampa.Storage.set('jackett_key', '');
+                Lampa.Storage.set('jackett_interview', 'all');
+                Lampa.Storage.set('parse_in_search', true);
+                Lampa.Storage.set('parse_lang', 'lg');
+                break;
+                
+            case 'jac_black':
+                Lampa.Storage.set('jackett_url', 'jacblack.ru:9117');
+                Lampa.Storage.set('jackett_key', '');
+                Lampa.Storage.set('jackett_interview', 'false');
+                Lampa.Storage.set('parse_in_search', true);
+                Lampa.Storage.set('parse_lang', 'lg');
+                break;
+                
+            case 'jacred_viewbox_dev':
+                Lampa.Storage.set('jackett_url', 'jacred.viewbox.dev');
+                Lampa.Storage.set('jackett_key', '777');
+                Lampa.Storage.set('jackett_interview', 'false');
+                Lampa.Storage.set('parse_in_search', true);
+                Lampa.Storage.set('parse_lang', 'lg');
+                break;
         }
     }
 
+    // Основное меню настроек парсера
     Lampa.Settings.main({
         component: 'parser',
         param: {
             name: 'jackett_urltwo',
             type: 'select',
             values: {
-                'no_parser': 'no_parser',
+                'no_parser': 'Без парсера',
                 'jacred_xyz': 'Jacred.xyz',
                 'jr_maxvol_pro': 'Jacred Maxvol Pro',
                 'jacred_ru': 'Jacred RU',
@@ -156,45 +161,37 @@
                     Lampa.Settings.main();
                 });
                 
-                if (localStorage.removeItem('jackett_urltwo') !== 'undefined') {
-                    $('.empty__title').hide();
-                    $('[data-name="jackett_key"]').hide();
-                    Lampa.Controller.toggle('settings_component');
-                }
-                
-                if (Lampa.Storage.field('parser') && Lampa.Storage.field('parser_use') == 'jackett') {
+                if (Lampa.Storage.field('parser_use') == 'jackett') {
                     html.show();
                     $(html.find('.selector'), html).css('color', '#ffffff');
-                    $('div[data-name="jackett_url"]').after('<div class="settings-param selector" data-name="jackett_urltwo" data-static="true">Выбрать парсер</div>');
                 } else {
                     html.hide();
                 }
-            }, 5);
+            }, 100);
         }
     });
 
-    Lampa.Settings.main_context({
-        'component': 'torrents',
-        'name': 'parser_torrent_type',
-        'before': '[data-name="jackett_url2"]',
-        'html': '<div class="settings-folder selector" data-name="jackett_urltwo"><div>Выбрать парсер</div></div>',
-        'onBack': false,
-        'onSelect': false
-    });
-
+    // Следим за изменением parser_use
     Lampa.Storage.listener.follow('parser_use', function(e) {
-        if (Lampa.Storage.field('parser_use') !== 'jackett') {
+        if (e.value !== 'jackett') {
             $('[data-name="jackett_key"]').hide();
+            $('[data-name="jackett_urltwo"]').hide();
         } else {
             $('[data-name="jackett_key"]').show();
-            $('div[data-name="jackett_urltwo"]').insertAfter('<div class="settings-param selector" data-name="jackett_urltwo" data-static="true">Выбрать парсер</div>');
+            setTimeout(function() {
+                var parserSelect = '<div class="settings-param selector" data-name="jackett_urltwo" data-static="true">Выбрать парсер</div>';
+                $('[data-name="jackett_url"]').after(parserSelect);
+            }, 100);
         }
     });
 
+    // Инициализация по умолчанию
     var initInterval = setInterval(function() {
-        if (typeof Lampa !== 'undefined') {
+        if (typeof Lampa !== 'undefined' && Lampa.Storage) {
             clearInterval(initInterval);
-            if (!Lampa.Storage.get('jack', false)) initDefault();
+            if (!Lampa.Storage.get('jack', false)) {
+                initDefault();
+            }
         }
     }, 100);
 
@@ -208,171 +205,124 @@
         Lampa.Storage.set('parse_lang', 'lg');
     }
 
+    // Функция показа меню выбора парсера
     function showParserMenu() {
-        var active = Lampa.Activity.active();
-        var html = [];
-        
-        html.push({
-            'title': 'Jacred.xyz',
-            'url': 'jacred.xyz',
-            'url_two': 'jacred_xyz',
-            'jac_key': '',
-            'jac_int': 'healthy',
-            'jac_lang': 'lg'
-        });
-        
-        html.push({
-            'title': 'Jacred Maxvol Pro',
-            'url': 'jr.maxvol.pro',
-            'url_two': 'jr_maxvol_pro',
-            'jac_key': '',
-            'jac_int': 'all',
-            'jac_lang': 'lg'
-        });
-        
-        html.push({
-            'title': 'Jacred RU',
-            'url': 'jac-red.ru',
-            'url_two': 'jacred_ru',
-            'jac_key': '',
-            'jac_int': 'false',
-            'jac_lang': 'lg'
-        });
-        
-        html.push({
-            'title': 'Viewbox',
-            'url': 'jacred.viewbox.dev',
-            'url_two': 'jacred_viewbox_dev',
-            'jac_key': '777',
-            'jac_int': 'false',
-            'jac_lang': 'lg'
-        });
-        
-        html.push({
-            'title': 'Jacred Pro',
-            'url': 'jacred.pro',
-            'url_two': 'jacred_pro',
-            'jac_key': '',
-            'jac_int': 'all',
-            'jac_lang': 'lg'
-        });
-        
-        html.push({
-            'title': 'Jac Black',
-            'url': 'jacblack.ru:9117',
-            'url_two': 'jac_black',
-            'jac_key': '',
-            'jac_int': 'false',
-            'jac_lang': 'lg'
-        });
+        var parsers = [
+            {
+                'title': 'Jacred.xyz',
+                'url': 'jacred.xyz',
+                'url_two': 'jacred_xyz',
+                'jac_key': '',
+                'jac_int': 'healthy',
+                'jac_lang': 'lg'
+            },
+            {
+                'title': 'Jacred Maxvol Pro',
+                'url': 'jr.maxvol.pro',
+                'url_two': 'jr_maxvol_pro',
+                'jac_key': '',
+                'jac_int': 'all',
+                'jac_lang': 'lg'
+            },
+            {
+                'title': 'Jacred RU',
+                'url': 'jac-red.ru',
+                'url_two': 'jacred_ru',
+                'jac_key': '',
+                'jac_int': 'false',
+                'jac_lang': 'lg'
+            },
+            {
+                'title': 'Viewbox',
+                'url': 'jacred.viewbox.dev',
+                'url_two': 'jacred_viewbox_dev',
+                'jac_key': '777',
+                'jac_int': 'false',
+                'jac_lang': 'lg'
+            },
+            {
+                'title': 'Jacred Pro',
+                'url': 'jacred.pro',
+                'url_two': 'jacred_pro',
+                'jac_key': '',
+                'jac_int': 'all',
+                'jac_lang': 'lg'
+            },
+            {
+                'title': 'Jac Black',
+                'url': 'jacblack.ru:9117',
+                'url_two': 'jac_black',
+                'jac_key': '',
+                'jac_int': 'false',
+                'jac_lang': 'lg'
+            }
+        ];
 
-        checkStatus(html).then(function(items) {
-            Lampa.Select.show({
-                'title': 'Выбрать парсер',
-                'items': items.map(function(item) {
-                    return {
-                        'title': item.title,
-                        'url': item.url,
-                        'url_two': item.url_two,
-                        'jac_key': item.jac_key,
-                        'jac_int': item.jac_int,
-                        'jac_lang': item.jac_lang
-                    };
-                }),
-                'onBack': function() {
-                    Lampa.Controller.toggle(active.render());
-                },
-                'onSelect': function(a) {
-                    Lampa.Storage.set('jackett_url', a.url);
-                    Lampa.Storage.set('jackett_urltwo', a.url_two);
-                    Lampa.Storage.set('jackett_key', a.jac_key);
-                    Lampa.Storage.set('jackett_interview', a.jac_int);
-                    Lampa.Storage.set('parse_lang', a.jac_lang);
-                    Lampa.Storage.set('parse_in_search', true);
-                    Lampa.Activity.back(active.render());
-                    
-                    var url = Lampa.Storage.field('url');
+        Lampa.Select.show({
+            title: 'Выбрать парсер',
+            items: parsers,
+            onBack: function() {
+                Lampa.Controller.toggle(Lampa.Activity.active().render());
+            },
+            onSelect: function(item) {
+                Lampa.Storage.set('jackett_url', item.url);
+                Lampa.Storage.set('jackett_urltwo', item.url_two);
+                Lampa.Storage.set('jackett_key', item.jac_key);
+                Lampa.Storage.set('jackett_interview', item.jac_int);
+                Lampa.Storage.set('parse_lang', item.jac_lang);
+                Lampa.Storage.set('parse_in_search', true);
+                
+                Lampa.Activity.back();
+                setTimeout(function() {
+                    window.location.reload();
+                }, 1000);
+            }
+        });
+    }
+
+    // Перехват клика по элементу выбора парсера
+    $(document).on('hover:enter', '[data-name="jackett_urltwo"]', function() {
+        showParserMenu();
+    });
+
+    // Observer для динамического добавления элементов
+    var observer;
+    function initObserver() {
+        if (observer) observer.disconnect();
+        
+        observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.type === 'childList' && Lampa.Storage.field('parser_use') === 'jackett') {
                     setTimeout(function() {
-                        window.location.reload();
-                    }, 1000);
-                    setTimeout(function() {
-                        Lampa.Noty.show(url);
-                    }, 2000);
+                        if ($('[data-name="jackett_urltwo"]').length === 0) {
+                            var parserSelect = '<div class="settings-param selector" data-name="jackett_urltwo" data-static="true">Выбрать парсер</div>';
+                            $('[data-name="jackett_url"]').after(parserSelect);
+                        }
+                    }, 100);
                 }
             });
-        }).catch(function(e) {
-            console.error('Error:', e);
         });
+        
+        observer.observe(document.body, { childList: true, subtree: true });
     }
 
-    function checkStatus(items) {
-        var promises = [];
-        for (var i = 0; i < items.length; i++) {
-            var url = items[i].url;
-            promises.push(checkSingleStatus(url, items[i].title, items[i]));
-        }
-        return Promise.all(promises);
-    }
-
-    function checkSingleStatus(url, title, item) {
-        return new Promise(function(resolve) {
-            var useProtocol = location.protocol === 'https:' ? 'https://' : 'http://';
-            var apiKey = '';
-            
-            if (url == 'jr.maxvol.pro') useProtocol = 'https://';
-            
-            var requestUrl = useProtocol + url + '/api/v2.0/indexers/status:healthy/results?apikey=' + apiKey;
-            var xhr = new XMLHttpRequest();
-            
-            xhr.open('GET', requestUrl, true);
-            xhr.timeout = 3000;
-            
-            xhr.ontimeout = function() {
-                item.title = '<span style="color:#ff2121;">✘&nbsp;&nbsp;' + title + '</span>';
-                resolve(item);
-            };
-            
-            xhr.onerror = function() {
-                item.title = '<span style="color:#ff2121;">✘&nbsp;&nbsp;' + title + '</span>';
-                resolve(item);
-            };
-            
-            xhr.onload = function() {
-                item.title = '<span style="color:#ff2121;">✘&nbsp;&nbsp;' + title + '</span>';
-                resolve(item);
-            };
-            
-            xhr.send();
-        });
-    }
-
-    var observer;
-    Lampa.Storage.listener.follow('parser_use', function(e) {
-        if (e.value == 'url') {
-            if (Lampa.Activity.active().activity_type == 'settings') initObserver();
-            else stopObserver();
+    // Запуск observer при активации настроек
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type == 'ready' && Lampa.Activity.active().activity_type == 'settings') {
+            if (Lampa.Storage.field('parser_use') == 'jackett') {
+                initObserver();
+            }
         }
     });
 
-    function initObserver() {
-        stopObserver();
-        var body = document.body;
-        var config = { childList: true, subtree: true };
-        observer = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                if ($('div[data-children="parser"]').length && Lampa.Storage.field('parser_use') == 'url') {
-                    showParserMenu();
-                    stopObserver();
-                }
-            });
-        });
-        observer.observe(body, config);
-    }
-
-    function stopObserver() {
-        if (observer) {
-            observer.disconnect();
-            observer = null;
+    // Остановка observer
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type == 'destroy') {
+            if (observer) {
+                observer.disconnect();
+                observer = null;
+            }
         }
-    }
+    });
+
 })();
