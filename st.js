6114f0c050e96fa508a5a00de50ddbb803af415b
alex <silver398@list.ru>
2025-10-27 22:57:32 +0300

    Add files via upload

---
+++ st.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+    Lampa.Platform.tv();
+
+    (function () {
+        'use strict';
+
+        function init() {
+            // Подписка на событие для обработки заблокированных запросов
+            Lampa.Listener.follow('request_secuses', function (event) {
+                if (event.data.origin) {
+                    var activeActivity = Lampa.Activity.active();
+                    activeActivity.source = 'dcma';
+                    Lampa.Storage.set('source', 'dcma', true);
+                    Lampa.Activity.replace(activeActivity);
+                    Lampa.Storage.set('source', 'cub', true);
+
+                }
+            });
+
+            // Интервал для проверки и модификации настроек
+            var interval = setInterval(function () {
+                if (typeof window.lampa_settings !== 'undefined' && (window.lampa_settings.fixdcma || window.lampa_settings.dcma)) {
+                    clearInterval(interval);
+                    if (window.lampa_settings.dcma) {
+                        window.lampa_settings.dcma = false;
+                    }
+                }
+            }, 100);
+        }
+
+        // Запуск init, если приложение готово, иначе прослушивание события 'app' для события ready
+        if (window.appready) {
+            init();
+        } else {
+            Lampa.Listener.follow('app', function (event) {
+                if (event.type === 'ready') {
+                    init();
+                }
+            });
+        }
+    })();
+})();
