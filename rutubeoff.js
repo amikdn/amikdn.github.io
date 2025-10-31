496bc7df3c0ce08e40c6228d4e3e767953ec0767
alex <silver398@list.ru>
2025-10-31 21:54:43 +0300

    rutubeoff.js

--- rutubeoff.js
+++ rutubeoff.js
@@ -6,7 +6,10 @@
                 var render = e.object.activity.render();
                 render.find('.view--rutube_trailer').remove();
                 render.find('.view--trailer').remove();
+                render.find('.online-prestige-watched').remove();
+                render.find('.watched-history').remove();
             } catch (err) {}
         }
     });
 })();
+
