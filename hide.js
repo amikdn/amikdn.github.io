779580166bdb25e94e6ef53b21686444c2bf064b
alex <silver398@list.ru>
2025-10-31 19:30:17 +0300

    hide.js

--- hide.js
+++ hide.js
@@ -1,30 +1,34 @@
 (function() {
   'use strict';
 
+  // Манифест плагина
   var manifst = {
     type: 'other',
-    version: '1.0',
+    version: '1.1',
     name: 'Скрыть историю в Lampac и Торрентах',
-    description: 'Скрывает блок с информацией о предыдущем просмотре (балансер, голос, сезон, эпизод) в плагине Lampac (онлайн) и в торрент-просмотре в Lampa'
+    description: 'Скрывает блок с информацией о предыдущем просмотре (балансер, голос, сезон, эпизод) в плагине Lampac (онлайн) и в торрент-просмотре в Lampa. Добавлено скрытие дополнительных классов для надежности.'
   };
 
   Lampa.Manifest.plugins = manifst;
 
+  // Функция запуска плагина
   function startPlugin() {
+    // Добавляем шаблон с CSS для скрытия блоков
     Lampa.Template.add('hide_lampa_history_css', `
       <style>
-        .online-prestige-watched {
-          display: none !important;
-        }
-        .torrent-prestige-watched {
+        .online-prestige-watched,
+        .torrent-prestige-watched,
+        .prestige-watched {
           display: none !important;
         }
       </style>
     `);
 
+    // Вставляем CSS в тело страницы
     $('body').append(Lampa.Template.get('hide_lampa_history_css', {}, true));
   }
 
+  // Запускаем плагин, если он ещё не запущен
   if (!window.hide_lampa_history) {
     window.hide_lampa_history = true;
     startPlugin();
