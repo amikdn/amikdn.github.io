ccad2cb75538a2a40fea34ffb3d28cadec69fffc
alex <silver398@list.ru>
2025-10-31 18:58:36 +0300

    t.js

--- t.js
+++ t.js
@@ -10,12 +10,6 @@
         path_api: 'apitmdb.' + (Lampa.Manifest && Lampa.Manifest.cub_domain ? Lampa.Manifest.cub_domain : 'cub.red') + '/3/'
     };
 
- //   function account(url){
- //       var email = Lampa.Storage.get('account_email')
- //       if(email) url = Lampa.Utils.addUrlComponent(url,'account_email=' + encodeURIComponent(email))
- //       return url
- //   }
-
     Lampa.TMDB.image = function (url) {
         var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
         return Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_image + url : base;
