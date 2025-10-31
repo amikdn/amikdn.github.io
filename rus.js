cfb48f46bec0dc7b00a468776935033d60dbb636
alex <silver398@list.ru>
2025-10-31 23:59:32 +0300

    rus.js

--- rus.js
+++ rus.js
@@ -5,17 +5,19 @@
 
     Lampa.Platform.tv();
 
-    // Иконка
-    function getMenuIcon() {
+    // === ИКОНКА ===
+    function getIcon() {
         return '<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-width="4"><path stroke-linejoin="round" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path stroke-linejoin="round" d="M24 18a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm0 18a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm-9-9a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm18 0a3 3 0 1 0 0-6a3 3 0 0 0 0 6Z"/><path stroke-linecap="round" d="M24 44h20"/></g></svg>';
     }
 
-    const currentDate = new Date().toISOString().substr(0, 10);
+    const API_KEY = '4ef0d7355d9ffb5151e987764708ce96';
+    const BASE_URL = 'https://api.themoviedb.org/3';
+    const currentDate = new Date().toISOString().split('T')[0];
 
-    // Подборки (с url)
+    // === ПОДБОРКИ ===
     const collections = [
-        { title: 'Русские фильмы', img: 'https://bylampa.github.io/img/rus_movie.jpg', url: 'discover/movie?with_original_language=ru&sort_by=primary_release_date.desc&primary_release_date.lte=' + currentDate },
-        { title: 'Русские сериалы', img: 'https://bylampa.github.io/img/rus_tv.jpg', url: 'discover/tv?with_original_language=ru&sort_by=first_air_date.desc&air_date.lte=' + currentDate },
+        { title: 'Русские фильмы', img: 'https://bylampa.github.io/img/rus_movie.jpg', url: `discover/movie?sort_by=primary_release_date.desc&with_original_language=ru&vote_average.gte=5&vote_average.lte=9.5&primary_release_date.lte=${currentDate}` },
+        { title: 'Русские сериалы', img: 'https://bylampa.github.io/img/rus_tv.jpg', url: `discover/tv?with_original_language=ru&sort_by=first_air_date.desc&air_date.lte=${currentDate}` },
         { title: 'СТС', img: 'https://bylampa.github.io/img/sts.jpg', url: 'discover/tv?with_networks=2493&sort_by=first_air_date.desc' },
         { title: 'ТНТ', img: 'https://bylampa.github.io/img/tnt.jpg', url: 'discover/tv?with_networks=2859&sort_by=first_air_date.desc' },
         { title: 'KION', img: 'https://bylampa.github.io/img/kion.jpg', url: 'discover/tv?with_networks=4085&sort_by=first_air_date.desc' },
@@ -27,11 +29,10 @@
         { title: 'Premier', img: 'https://bylampa.github.io/img/premier.jpg', url: 'discover/tv?with_networks=1191&sort_by=first_air_date.desc' }
     ];
 
-    // === КОМПОНЕНТ (ИСПРАВЛЕННЫЙ render + activity) ===
+    // === КОМПОНЕНТ rus_movie ===
     function rusComponent(params) {
         const component = new Lampa.Component(params);
 
-        // Главная — список подборок
         component.create = function() {
             const data = {
                 title: 'Русское',
@@ -45,20 +46,22 @@
             this.build(data);
         };
 
-        // Загрузка страницы
-        component.nextPageReuest = function(p, success, error) {
+        // Загрузка TMDB
+        component.nextPageReuest = function(p, onSuccess, onError) {
+            const url = `${BASE_URL}/${p.url}&api_key=${API_KEY}&language=ru&page=${p.page || 1}`;
+            console.log('[Русское] Загружаю:', url);
+
             const network = new Lampa.Reguest();
-            const url = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + p.url + '&page=' + (p.page || 1);
             network.native(url, res => {
                 res.title = p.title || 'Подборка';
-                success(res);
-            }, error);
+                onSuccess(res);
+            }, onError);
         };
 
         // КЛИК ПО КАРТОЧКЕ — РАБОТАЕТ!
         component.activity = function(card) {
             card.onEnter = () => {
-                console.log('[Русское] Открываю подборку:', card.title);
+                console.log('[Русское] Открываю:', card.title);
                 Lampa.Activity.push({
                     url: card.url,
                     title: card.title,
@@ -75,12 +78,12 @@
     Lampa.Component.add('rus_movie', rusComponent);
 
     // === МЕНЮ ===
-    function addMenu() {
+    function addMenuItem() {
         $('.menu__item[data-rus]').remove();
 
         const $item = $(`
             <li class="menu__item selector" data-rus="true">
-                <div class="menu__ico">${getMenuIcon()}</div>
+                <div class="menu__ico">${getIcon()}</div>
                 <div class="menu__text">Русское</div>
             </li>
         `);
@@ -99,14 +102,14 @@
         if ($menu.length) {
             $menu.append($item);
             $item.find('.menu__text').css('text-align', 'center');
-            console.log('[Русское] Пункт в меню добавлен');
+            console.log('[Русское] Пункт добавлен');
         }
     }
 
-    // Гарантия загрузки
-    $(document).ready(() => setTimeout(addMenu, 1000));
-    setTimeout(addMenu, 3000);
-    setInterval(addMenu, 10000);
+    // Гарантия
+    $(document).ready(() => setTimeout(addMenuItem, 1000));
+    setTimeout(addMenuItem, 3000);
+    setInterval(addMenuItem, 10000);
 
     // === НАСТРОЙКИ ===
     Lampa.SettingsApi.addParam({
@@ -119,11 +122,11 @@
     if (!Lampa.Manifest.plugins) Lampa.Manifest.plugins = {};
     Lampa.Manifest.plugins.rus_movie = {
         type: 'movie',
-        version: '1.5.0',
+        version: '2.0.0',
         name: 'Русское',
-        description: 'Полностью рабочие подборки русских фильмов и сериалов',
+        description: 'Рабочие русские подборки через TMDB',
         component: 'rus_movie'
     };
 
-    console.log('[Русское] Плагин готов');
+    console.log('[Русское] Готово');
 })();
