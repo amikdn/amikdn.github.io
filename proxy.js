b1efde4ec62188b9942d744e5630f51957bd1822
alex <silver398@list.ru>
2025-11-03 13:37:52 +0300

    proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,37 @@
     'use strict';
     Lampa.Platform.tv();
 
+    // Plugin definition for TMDB proxy
+    var plugin = {
+        name: 'TMDB My Proxy with Fixes',
+        version: '1.0.1',
+        description: 'Проксирование постеров и API сайта TMDB с дополнительными исправлениями',
+        path_image: Lampa.Utils.protocol() + 'tmdbimg.bylampa.online/',
+        path_api: Lampa.Utils.protocol() + 'tmdbapi.bylampa.online/3/'
+    };
+
+    // Override TMDB image URL
+    Lampa.TMDB.image = function (url) {
+        var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+        return Lampa.Storage.field('proxy_tmdb') ? plugin.path_image + url : base;
+    };
+
+    // Override TMDB API URL
+    Lampa.TMDB.api = function (url) {
+        var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+        return Lampa.Storage.field('proxy_tmdb') ? plugin.path_api + url : base;
+    };
+
+    // Remove proxy settings element in TMDB settings
+    Lampa.Settings.listener.follow('open', function (e) {
+        if (e.name === 'tmdb') {
+            e.body.find('[data-parent="proxy"]').remove();
+        }
+    });
+
+    // Initialization for handling blocked requests and settings modifications
     function init() {
+        // Subscribe to event for handling blocked requests
         Lampa.Listener.follow('request_secuses', function (event) {
             if (event.data.blocked) {
                 var activeActivity = Lampa.Activity.active();
@@ -13,6 +43,7 @@
             }
         });
 
+        // Interval for checking and modifying settings
         var interval = setInterval(function () {
             if (typeof window.lampa_settings !== 'undefined' && (window.lampa_settings.fixdcma || window.lampa_settings.dcma)) {
                 clearInterval(interval);
@@ -23,6 +54,7 @@
         }, 100);
     }
 
+    // Run init if app is ready, otherwise listen for ready event
     if (window.appready) {
         init();
     } else {
@@ -32,28 +64,4 @@
             }
         });
     }
-
-    var tmdb_proxy = {
-        name: 'TMDB Proxy',
-        version: '1.0.3',
-        description: 'Проксирование постеров и API сайта TMDB',
-        path_image: 'tmdbimage.abmsx.tech/',
-        path_api: 'apitmdb.' + (Lampa.Manifest && Lampa.Manifest.cub_domain ? Lampa.Manifest.cub_domain : 'cub.red') + '/3/'
-    };
-
-    Lampa.TMDB.image = function (url) {
-        var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
-        return Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_image + url : base;
-    };
-
-    Lampa.TMDB.api = function (url) {
-        var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
-        return Lampa.Storage.field('proxy_tmdb') ? '//tmdb.abmsx.tech/3/' + url : base;
-    };
-
-    Lampa.Settings.listener.follow('open', function (e) {
-        if (e.name == 'tmdb') {
-            e.body.find('[data-parent="proxy"]').remove();
-        }
-    });
 })();
