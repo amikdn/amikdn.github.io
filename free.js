e1d3c93f1ef9f3fa9fc745b7d62f9ae017a55647
alex <silver398@list.ru>
2025-11-28 19:27:47 +0300

    free.js

--- free.js
+++ free.js
@@ -1,7 +1,36 @@
 (function () {
     'use strict';
 
-    //for Lampa.
+    // TMDB Proxy Plugin Configuration
+    Lampa.Platform.tv();
+    var plugin = {
+        name: 'TMDB My Proxy with DMCA Disable',
+        version: '1.0.2',
+        description: 'Проксирование постеров и API сайта TMDB с отключением DMCA-фич'
+    };
+    plugin.path_image = Lampa.Utils.protocol() + 'tmdbimg.bylampa.online/';
+    plugin.path_api = Lampa.Utils.protocol() + 'tmdbapi.bylampa.online/3/';
+
+    // TMDB Image Proxy Function
+    Lampa.TMDB.image = function (url) {
+        var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+        return Lampa.Storage.field('proxy_tmdb') ? plugin.path_image + url : base;
+    };
+
+    // TMDB API Proxy Function
+    Lampa.TMDB.api = function (url) {
+        var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+        return Lampa.Storage.field('proxy_tmdb') ? plugin.path_api + url : base;
+    };
+
+    // Settings Listener for TMDB Proxy
+    Lampa.Settings.listener.follow('open', function (e) {
+        if (e.name === 'tmdb') {
+            e.body.find('[data-parent="proxy"]').remove();
+        }
+    });
+
+    // DMCA Disable Configuration for Lampa
     window.lampa_settings = window.lampa_settings || {};
     window.lampa_settings.dcma = false;
     window.lampa_settings.disable_features = window.lampa_settings.disable_features || {};
