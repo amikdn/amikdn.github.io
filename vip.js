62d99674af75360d6cda7c2a9624e6fb276ddc77
alex <silver398@list.ru>
2025-12-28 01:35:57 +0300

    vip.js

--- vip.js
+++ vip.js
@@ -73,7 +73,7 @@
               video.currentTime = video.duration || 99999;
               video.dispatchEvent(new Event('ended'));
               video.dispatchEvent(new Event('timeupdate'));
-            }, 1);
+            }, 10);
           };
           return video;
         }
@@ -123,4 +123,3 @@
     }
   })();
 })();
-
