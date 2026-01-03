f28587bce3a8753eb2375dccca56d424cef53053
alex <silver398@list.ru>
2026-01-03 21:40:16 +0300

    hub.js

--- hub.js
+++ hub.js
@@ -20,7 +20,7 @@
     function _0x559ec2() {
       if (Lampa.Manifest.origin !== "lampa") {
         Lampa.Noty.show("Ошибка доступа");
-        return;
+
       }
       var _0x175cd0 = function (_0x2d965e) {
         var _0x287088 = _0x2d965e.card || _0x2d965e;
