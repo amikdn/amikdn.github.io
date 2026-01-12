a0c6a24c1ca9786c4e25b96c7d0378ee8ce0292e
alex <silver398@list.ru>
2026-01-12 19:51:02 +0300

    Update vip.js

--- vip.js
+++ vip.js
@@ -294,8 +294,6 @@
             // Меню слева
             ".menu__item.focus {border-radius: 0 0.5em 0.5em 0;\n}\n" +
             ".menu__list {\npadding-left: 0em;\n}\n" +
-            // Сохраняем исходный цвет иконок при фокусе (отменяем наследование белого цвета)
-            ".menu__item:not(.focus) .menu__ico,\n.menu__item.focus .menu__ico {\ncolor: rgba(255, 255, 255, 0.7) !important;\n}\n" +
             // Белые иконки в бошке
             // ".head__action.focus, .head__action.hover {\ncolor: fff;\n}\n" +
             "</style>\n";
