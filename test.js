e2c962625a07106f786f7583393447e909fecab4
alex <silver398@list.ru>
2026-01-16 04:30:59 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    const fixedUnicId = 'netwhiz';
+   // const fixedLampaUid = 'xgGax1Fs';
+
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type === 'ready') {
+            Lampa.Storage.set('lampac_unic_id', fixedUnicId);
+            Lampa.Storage.set('lampa_uid', fixedLampaUid);
+            Lampa.Storage.set('platform', 'browser');
+            console.log('ID и platform установлены:', fixedUnicId, fixedLampaUid, 'browser');
+        }
+    });
+})();
