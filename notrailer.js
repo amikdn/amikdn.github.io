935670a238e7f04d84dccb88afc5353d2e68c4da
alex <silver398@list.ru>
2026-01-17 13:00:16 +0300

    Update notrailer.js

--- notrailer.js
+++ notrailer.js
@@ -2,7 +2,8 @@
     'use strict';	
     Lampa.Listener.follow('full', function (e) {
         if (e.type == 'complite') {
-            e.object.activity.render().find('.view--trailer').remove();
+            e.object.activity.render().find('.shots-view-button').remove();
         }
     });
 })();
+
