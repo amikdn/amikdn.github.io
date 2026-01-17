7c2466ebcba003c8b5dfd4177641a04d5e824063
alex <silver398@list.ru>
2026-01-17 12:41:21 +0300

    Update button2.js

--- button2.js
+++ button2.js
@@ -1,54 +1,9 @@
 (function () {
     'use strict';
     
-    // Функция для удаления пустых контейнеров
-    function removeEmptyContainers() {
-        // Удаляем пустые scroll__body контейнеры
-        var scrollBodies = document.querySelectorAll('.scroll__body');
-        for (var i = 0; i < scrollBodies.length; i++) {
-            var scrollBody = scrollBodies[i];
-            var hasItemsLine = scrollBody.querySelector('.items-line');
-            if (!hasItemsLine && scrollBody.children.length === 0) {
-                if (scrollBody.parentNode) {
-                    scrollBody.parentNode.removeChild(scrollBody);
-                } else {
-                    scrollBody.remove();
-                }
-            }
-        }
-        
-        // Удаляем пустые scroll__content контейнеры
-        var scrollContents = document.querySelectorAll('.scroll__content');
-        for (var j = 0; j < scrollContents.length; j++) {
-            var scrollContent = scrollContents[j];
-            var hasItemsLineInContent = scrollContent.querySelector('.items-line');
-            var hasScrollBody = scrollContent.querySelector('.scroll__body');
-            if (!hasItemsLineInContent && (!hasScrollBody || scrollContent.querySelector('.scroll__body').children.length === 0)) {
-                if (scrollContent.children.length === 0 && scrollContent.parentNode) {
-                    scrollContent.parentNode.removeChild(scrollContent);
-                }
-            }
-        }
-        
-        // Удаляем пустые scroll контейнеры
-        var scrolls = document.querySelectorAll('.scroll');
-        for (var k = 0; k < scrolls.length; k++) {
-            var scroll = scrolls[k];
-            var hasItemsLineInScroll = scroll.querySelector('.items-line');
-            if (!hasItemsLineInScroll && scroll.children.length === 0) {
-                if (scroll.parentNode) {
-                    scroll.parentNode.removeChild(scroll);
-                } else {
-                    scroll.remove();
-                }
-            }
-        }
-    }
-    
     // Функция для удаления items-line с Shots
     function removeShotsItemsLine() {
         var itemsLines = document.querySelectorAll('.items-line');
-        var removed = false;
         
         for (var i = 0; i < itemsLines.length; i++) {
             var itemsLine = itemsLines[i];
@@ -62,7 +17,6 @@
                 } else {
                     itemsLine.remove();
                 }
-                removed = true;
                 continue;
             }
             
@@ -75,13 +29,7 @@
                 } else {
                     itemsLine.remove();
                 }
-                removed = true;
             }
-        }
-        
-        // Если что-то удалили, проверяем и удаляем пустые контейнеры
-        if (removed) {
-            removeEmptyContainers();
         }
     }
     
@@ -116,19 +64,12 @@
             }
         }
         if (shouldCheck) {
-            setTimeout(function() {
-                removeShotsItemsLine();
-                removeEmptyContainers();
-            }, 0);
+            setTimeout(removeShotsItemsLine, 0);
         }
     });
     
     // Запускаем проверку периодически (чаще при прокрутке)
-    var checkInterval = setInterval(function() {
-        removeShotsItemsLine();
-        // Также периодически проверяем пустые контейнеры
-        removeEmptyContainers();
-    }, 150);
+    var checkInterval = setInterval(removeShotsItemsLine, 150);
     
     // Начинаем наблюдение после загрузки
     if (document.body) {
@@ -149,5 +90,4 @@
     setTimeout(removeShotsItemsLine, 50);
     setTimeout(removeShotsItemsLine, 200);
     setTimeout(removeShotsItemsLine, 500);
-    setTimeout(removeEmptyContainers, 600);
 })();
