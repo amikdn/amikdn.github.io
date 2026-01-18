513f57fe00132f1f4da40bf5907e720feced2eb8
alex <silver398@list.ru>
2026-01-19 00:28:45 +0300

    Update noshots.js

--- noshots.js
+++ noshots.js
@@ -1,44 +1,34 @@
 (function () {
     'use strict';
 
-    console.log('[Shots Blocker] Loaded');
-
-    // Блокируем регистрацию компонентов
     const origAddComponent = Lampa.Component.add;
-    Lampa.Component.add = function(name, comp){
-        if(name.startsWith('shots_')){
-            console.log('[Shots Blocker] Blocked component:', name);
+    Lampa.Component.add = function (name, comp) {
+        if (name.startsWith('shots_')) {
             return;
         }
         return origAddComponent.apply(this, arguments);
     };
 
-    // Блокируем добавление строк на главные экраны
     const origContentAdd = Lampa.ContentRows.add;
-    Lampa.ContentRows.add = function(obj){
-        if(obj.name === 'shots_main' || 
-           (obj.title && obj.title === 'Shots')){
-            console.log('[Shots Blocker] Blocked content row: Shots');
+    Lampa.ContentRows.add = function (obj) {
+        if (obj.name === 'shots_main' ||
+            (obj.title && obj.title === 'Shots')) {
             return;
         }
         return origContentAdd.apply(this, arguments);
     };
 
-    // Блокируем кнопку меню Shots
     const origMenuAdd = Lampa.Menu.addButton;
-    Lampa.Menu.addButton = function(icon, title, callback){
-        if(title === 'Shots'){
-            console.log('[Shots Blocker] Blocked menu button: Shots');
+    Lampa.Menu.addButton = function (icon, title, callback) {
+        if (title === 'Shots') {
             return;
         }
         return origMenuAdd.apply(this, arguments);
     };
 
-    // На всякий случай — убираем уже добавленную кнопку
-    setTimeout(()=>{
-        try{
+    setTimeout(() => {
+        try {
             Lampa.Menu.remove && Lampa.Menu.remove('Shots');
-        }catch(e){}
-    },1000);
-
+        } catch (e) {}
+    }, 1000);
 })();
