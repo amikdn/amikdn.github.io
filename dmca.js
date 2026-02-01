149e00472763762e5c1523ea4af954015cfcd211
alex <silver398@list.ru>
2026-02-01 04:19:18 +0300

    Add files via upload

---
+++ dmca.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+
+    function start() {
+        if (window.anti_dmca_plugin) {
+            return;
+        }
+
+        window.anti_dmca_plugin = true;
+        window.lampa_settings.dcma = [];
+
+        Lampa.Utils.dcma = function () { return undefined };
+        var defaultSource = Lampa.Storage.get('source', 'cub');
+
+        Lampa.Listener.follow('request_secuses', function (event) {
+            if (event.data.blocked) {
+                window.lampa_settings.dcma = [];
+
+                var active = Lampa.Activity.active();
+                active.source = 'tmdb';
+                Lampa.Storage.set('source', 'tmdb', true);
+                Lampa.Activity.replace(active);
+                Lampa.Storage.set('source', defaultSource, true);
+                setTimeout(function() {
+                    Lampa.Controller.toggle('content');
+                }, 250);
+            }
+        });
+    }
+
+    if (window.appready) {
+        start();
+    } else {
+        Lampa.Listener.follow('app', function (event) {
+            if (event.type === 'ready') {
+                start();
+            }
+        });
+    }
+})();
