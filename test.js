7a09d8470771769b183951916c20d6ed7f035d82
alex <silver398@list.ru>
2026-02-10 16:52:55 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
-(function() {
-    'use strict';
-
-    const fixedUnicId = 'raxmind';
+//(function() {
+//    'use strict';
+//
+//    const fixedUnicId = 'raxmind';
    // const fixedLampaUid = 'xgGax1Fs';
 
-    Lampa.Listener.follow('app', function(e) {
-        if (e.type === 'ready') {
-            Lampa.Storage.set('lampac_unic_id', fixedUnicId);
+//    Lampa.Listener.follow('app', function(e) {
+//        if (e.type === 'ready') {
+//            Lampa.Storage.set('lampac_unic_id', fixedUnicId);
       //      Lampa.Storage.set('lampa_uid', fixedLampaUid);
       //      Lampa.Storage.set('platform', 'browser');
       //      console.log('ID и platform установлены:', fixedUnicId, fixedLampaUid, 'browser');
-        }
-    });
+//        }
+//    });
 
-})();
+//})();
+
 
 
 
