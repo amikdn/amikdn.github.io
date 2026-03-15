5a51868ea3f9082820bf8bdfeb7fe53515a45812
alex <silver398@list.ru>
2026-03-15 18:43:28 +0300

    Update buttons2.js

--- buttons2.js
+++ buttons2.js
@@ -559,6 +559,9 @@
             return 'showy_online_button';
         }
         var viewClasses = classes.split(' ').filter(function(c) { return c.indexOf('view--') === 0 || c.indexOf('button--') === 0; }).join('_');
+        if (classes.indexOf('lampac--button') !== -1 && subtitle) {
+            viewClasses = classes.split(' ').filter(function(c) { return c.indexOf('view--') === 0; }).join('_');
+        }
         if (!viewClasses && !text) {
             return 'button_unknown';
         }
@@ -600,9 +603,18 @@
             var $btn = $(this);
             if (isExcluded($btn)) return;
             var btnId = getButtonId($btn);
-            if (processedIds[btnId]) return;
-            processedIds[btnId] = true;
+            if (processedIds[btnId]) {
+                if ($btn.hasClass('button--priority') && !processedIds[btnId].button.hasClass('button--priority')) {
+                    var prev = processedIds[btnId];
+                    var arr = categories[prev.type];
+                    var idx = arr.indexOf(prev.button);
+                    if (idx !== -1) arr[idx] = $btn;
+                    processedIds[btnId] = { button: $btn, type: prev.type };
+                }
+                return;
+            }
             var type = getButtonType($btn);
+            processedIds[btnId] = { button: $btn, type: type };
             if (categories[type]) {
                 categories[type].push($btn);
             } else {
