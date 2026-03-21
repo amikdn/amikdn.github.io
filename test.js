500317fb2e5aa1fd63784fe581bc6e403833e002
alex <silver398@list.ru>
2026-03-21 20:44:13 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-//(function() {
-//    'use strict';
-//
-//    const fixedUnicId = 'raxmind';
-   // const fixedLampaUid = 'xgGax1Fs';
+(function() {
+    'use strict';
 
-//    Lampa.Listener.follow('app', function(e) {
-//        if (e.type === 'ready') {
-//            Lampa.Storage.set('lampac_unic_id', fixedUnicId);
-      //      Lampa.Storage.set('lampa_uid', fixedLampaUid);
-      //      Lampa.Storage.set('platform', 'browser');
-      //      console.log('ID и platform установлены:', fixedUnicId, fixedLampaUid, 'browser');
-//        }
-//    });
+    const fixedUnicId = 'guest';
+    const fixedLampaUid = 'xgGax1Fs';
 
-//})();
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type === 'ready') {
+            Lampa.Storage.set('lampac_unic_id', fixedUnicId);
+            Lampa.Storage.set('lampa_uid', fixedLampaUid);
+            Lampa.Storage.set('platform', 'browser');
+            console.log('ID и platform установлены:', fixedUnicId, fixedLampaUid, 'browser');
+        }
+    });
+
+})();
 
