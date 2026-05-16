812d171c7c45f01a22129418e56d9b0a8d50e4a5
alex <silver398@list.ru>
2026-05-16 14:42:00 +0300

    Update q.js

--- q.js
+++ q.js
@@ -5,8 +5,8 @@
     var CACHE_DURATION = 24 * 60 * 60 * 1000; // Срок хранения кэша качества
     var CACHE_STORAGE_KEY = 'qualview_quality_cache';
     var PROTOCOL_JACRED = 'https://';
-    var DOMAIN_JACRED = 'jac.red';
-    var API_KEY_JACRED = ''; // Введите ключ при необходимости
+    var DOMAIN_JACRED = '5.252.116.77:11212';
+    var API_KEY_JACRED = '1'; // Введите ключ при необходимости
 
     // Функция преобразования качества
     function convertQuality(resolution) {
