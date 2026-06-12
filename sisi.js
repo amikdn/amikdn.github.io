dcc16199bbc649d63bf2dbf2ba683860f79734c5
alex <silver398@list.ru>
2026-06-12 17:49:19 +0300

    Update script mirror list in initExternalScriptLoader
    Removed some script mirror URLs from the list.

--- sisi.js
+++ sisi.js
@@ -6,13 +6,8 @@
   function initExternalScriptLoader() {
     // Список зеркал для загрузки внешнего скрипта sisi.js
     const scriptMirrors = [
-      'http://89.110.97.220:10254/sisi.js',
       'https://ab2024.ru/sisi.js',
-      'http://139.28.220.127:9118/sisi.js',
-      'http://185.121.235.124:11176/sisi.js',
       'http://144.124.225.106:11310/sisi.js',
-      'http://83.217.212.10:12128/sisi.js',
-      'http://91.184.245.56:9215/sisi.js',
     ];
 
     // Выбираем случайное зеркало
