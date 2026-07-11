5521b7ecfcd7676545e5f7e903b59afc826a6a1e
alex <silver398@list.ru>
2026-07-11 17:29:40 +0300

    Refactor credential handling in sh.js

--- sh.js
+++ sh.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
 
-    if (window.showy_only_loader) return;
-    window.showy_only_loader = true;
+    if (window.__s1) return;
+    window.__s1 = true;
 
     function decodeHidden(input) {
         try {
@@ -12,29 +12,31 @@
         }
     }
 
-    var pluginUrl = decodeHidden('aHR0cDovL3Nob3d5Lm9ubGluZS9vbmxpbmUuanM=');
-    var showyUid = decodeHidden('aThucWI5dnc=');
-    var showyToken = decodeHidden('ZjgzNzcwNTctOTBlYi00ZDc2LTkzYzktNzYwNTk1MmEwOTZs');
+    var k0 = decodeHidden('bGFtcGFjX3VuaWNfaWQ=');
+    var k1 = decodeHidden('c2hvd3lfdG9rZW4=');
+    var v0 = decodeHidden('aHR0cDovL3Nob3d5cHJvLmNvbS9vbmxpbmUuanM=');
+    var v1 = decodeHidden('aThucWI5dnc=');
+    var v2 = decodeHidden('ZjgzNzcwNTctOTBlYi00ZDc2LTkzYzktNzYwNTk1MmEwOTZs');
 
-    function lockCredentials() {
-        if (!Lampa.Storage.__showyOriginalSet) {
-            Lampa.Storage.__showyOriginalSet = Lampa.Storage.set;
+    function applyValues() {
+        if (!Lampa.Storage.__s0) {
+            Lampa.Storage.__s0 = Lampa.Storage.set;
             Lampa.Storage.set = function(name, value) {
-                if (name === 'lampac_unic_id') value = showyUid;
-                if (name === 'showy_token') value = showyToken;
-                return Lampa.Storage.__showyOriginalSet.call(Lampa.Storage, name, value);
+                if (name === k0) value = v1;
+                if (name === k1) value = v2;
+                return Lampa.Storage.__s0.call(Lampa.Storage, name, value);
             };
         }
 
-        Lampa.Storage.set('lampac_unic_id', showyUid);
-        Lampa.Storage.set('showy_token', showyToken);
+        Lampa.Storage.set(k0, v1);
+        Lampa.Storage.set(k1, v2);
     }
 
     function start() {
-        lockCredentials();
+        applyValues();
 
-        Lampa.Utils.putScript([pluginUrl], function() {
-            lockCredentials();
+        Lampa.Utils.putScript([v0], function() {
+            applyValues();
         }, false, function() {}, true);
     }
 
