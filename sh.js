896c050016b47f79973dda810e37621cb3a7994f
alex <silver398@list.ru>
2026-07-11 17:18:41 +0300

    Add files via upload

---
+++ sh.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    if (window.showy_only_loader) return;
+    window.showy_only_loader = true;
+
+    function decodeHidden(input) {
+        try {
+            return decodeURIComponent(escape(atob(input)));
+        } catch (e) {
+            return atob(input);
+        }
+    }
+
+    var pluginUrl = decodeHidden('aHR0cDovL3Nob3d5Lm9ubGluZS9vbmxpbmUuanM=');
+    var showyUid = decodeHidden('aThucWI5dnc=');
+    var showyToken = decodeHidden('ZjgzNzcwNTctOTBlYi00ZDc2LTkzYzktNzYwNTk1MmEwOTZs');
+
+    function start() {
+        Lampa.Storage.set('lampac_unic_id', showyUid);
+        Lampa.Storage.set('showy_token', showyToken);
+
+        Lampa.Utils.putScript([pluginUrl], function() {}, false, function() {}, true);
+    }
+
+    if (window.appready) {
+        start();
+    } else {
+        Lampa.Listener.follow('app', function(event) {
+            if (event.type === 'ready') start();
+        });
+    }
+})();
